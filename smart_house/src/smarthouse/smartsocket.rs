283a7a2fdc9a3b276ca30e4c350b2dcc27a53919
Kerosin3 <keromvp@gmail.com>
2022-11-17 14:11:27 +0300

    added unused code

--- smart_house/src/smarthouse/smartsocket.rs
+++ smart_house/src/smarthouse/smartsocket.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub struct SmartSocket;
 
 impl SmartSocket {
@@ -5,25 +6,31 @@ impl SmartSocket {
     ///
     /// # Examples
     ///
+    #[allow(dead_code)]
     pub fn new() -> SmartSocket {
         SmartSocket
     }
     /// power on the socket
     ///
 
+    #[allow(dead_code)]
     pub fn power_on(&mut self) {
         todo!();
     }
     /// power off the socket
     ///
+    #[allow(dead_code)]
     pub fn power_off(&mut self) {
         todo!()
     }
+    #[allow(dead_code)]
     /// get current consumed power
     ///
+    #[allow(dead_code)]
     pub fn get_cur_consum_power(&self) {
         todo!()
     }
+    #[allow(dead_code)]
     pub fn test_socket(&self) {
         println!("test smart socket");
     }
--- smart_house/src/smarthouse/smarttermometer.rs
+++ smart_house/src/smarthouse/smarttermometer.rs
@@ -1,16 +1,21 @@
+#[allow(dead_code)]
 pub struct SmartTermometer;
 
 impl SmartTermometer {
     /// get new instance of termometer
     ///
+    #[allow(dead_code)]
     pub fn new() -> SmartTermometer {
         SmartTermometer
     }
+    #[allow(dead_code)]
     /// get current temperature from instance
     ///
+    #[allow(dead_code)]
     pub fn get_cur_temp(&self) {
         todo!();
     }
+    #[allow(dead_code)]
     pub fn test_smart_temp(&self) {
         println!("test smart termometer");
     }

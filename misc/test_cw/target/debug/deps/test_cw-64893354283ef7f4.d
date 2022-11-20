4ec32168928506c6444345d1504156d385aacc9b
Kerosin3 <keromvp@gmail.com>
2022-11-20 18:37:54 +0300

    added traits v2

--- misc/test_cw/target/debug/deps/test_cw-64893354283ef7f4.d
+++
@@ -1,5 +0,0 @@
-/home/ker0/test/RUST/misc/test_cw/target/debug/deps/test_cw-64893354283ef7f4: src/main.rs
-
-/home/ker0/test/RUST/misc/test_cw/target/debug/deps/test_cw-64893354283ef7f4.d: src/main.rs
-
-src/main.rs:
--- misc/test_cw/target/debug/test_cw.d
+++
@@ -1,1 +0,0 @@
-/home/ker0/test/RUST/misc/test_cw/target/debug/test_cw: /home/ker0/test/RUST/misc/test_cw/src/main.rs

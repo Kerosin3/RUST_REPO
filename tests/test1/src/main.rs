ad0743f17d738baf45ddbd1d0271598d41113512
Kerosin3 <keromvp@gmail.com>
2022-12-23 17:36:12 +0300

    added test project

---
+++ tests/test1/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn test_me()-> bool {
+    true
+}
+#[cfg(test)]
+
+mod testing_example {
+        use super::*;
+        #[test]
+        fn t1() {
+            assert_eq!(test_me(),true);
+        }
+}
+

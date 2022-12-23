d6616fb4dd0bab4ec83f6689f384b20bb898efc1
Kerosin3 <keromvp@gmail.com>
2022-12-23 17:41:15 +0300

    fixed workflow v2

--- tests/test1/src/main.rs
+++ tests/test1/src/main.rs
@@ -2,16 +2,15 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn test_me()-> bool {
+fn test_me() -> bool {
     true
 }
 #[cfg(test)]
 
 mod testing_example {
-        use super::*;
-        #[test]
-        fn t1() {
-            assert_eq!(test_me(),true);
-        }
+    use super::*;
+    #[test]
+    fn t1() {
+        assert_eq!(test_me(), true);
+    }
 }
-

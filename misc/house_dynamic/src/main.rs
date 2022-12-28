eefb88144eb546af8a861d3b39da2d273511ddca
Kerosin3 <keromvp@gmail.com>
2022-12-28 14:15:27 +0300

    added hw5

--- misc/house_dynamic/src/main.rs
+++ misc/house_dynamic/src/main.rs
@@ -1,7 +1,9 @@
 #![feature(associated_type_defaults)]
 #![feature(is_some_and)]
 
+use std::any::Any;
 use std::cell::RefCell;
+use std::fmt;
 use std::process::Output;
 use std::rc::Rc;
 use std::rc::Weak;
@@ -28,6 +30,10 @@ pub enum ErrorC {
     ErrorSettingState,
     #[error("Cannot get state")]
     ErrorGettingState,
+    #[error("Some error ocurred")]
+    ErrorOther,
+    #[error("Error execute handler command")]
+    ErrorHandleOperation,
 }
 
 struct SmartHouse {
@@ -45,7 +51,7 @@ impl SmartHouse {
             devices: Box::new(RefCell::new(vec![])),
         })
     }
-    fn append_room(&mut self, a_room: &str) -> Result<(), ErrorC> {
+    pub fn append_room(&mut self, a_room: &str) -> Result<(), ErrorC> {
         if self.test_whether_room_exists(a_room).is_some() {
             Err(ErrorC::RoomExists(a_room.to_string()))
         } else {
@@ -57,6 +63,83 @@ impl SmartHouse {
         }
     }
 
+    fn get_device_number_in_room(&self, a_room: &str) -> Option<usize> {
+        if let Some(n_room) = self.test_whether_room_exists(a_room) {
+            let room_generic = self.rooms.get(n_room).unwrap().as_any();
+            if let Some(obj) = room_generic.downcast_ref::<Room_Generic>() {
+                Some(obj.devices.borrow().len())
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+    pub fn get_dev_report(&self, a_device: &Rc<RefCell<dyn Device>>) -> Option<String> {
+        let mut out_str = String::new();
+        let dev_name = a_device.borrow().get_name().clone();
+        for (_i, room) in self.rooms.iter().enumerate() {
+            if room.find_dev_name(&dev_name).is_some() {
+                fmt::write(
+                    &mut out_str,
+                    format_args!("room: {},device: {} ,", room.get_room_name(), dev_name),
+                )
+                .expect("error whiting string")
+            } else {
+                continue;
+            }
+        }
+        if out_str.is_empty() {
+            None
+        } else {
+            Some(out_str)
+        }
+    }
+    pub fn delete_device(&self, a_room: &str, a_device: &str) -> Result<(), ErrorC> {
+        if let Some(n_room) = self.test_whether_room_exists(a_room) {
+            let generic_room_obj = self.rooms.get(n_room).unwrap().as_any();
+            match generic_room_obj.downcast_ref::<Room_Generic>() {
+                // downcast to generic room
+                Some(obj) => {
+                    if let Some(dev_num) = obj.find_dev_name(a_device) {
+                        // find matched device
+                        obj.devices.as_ref().borrow_mut().swap_remove(dev_num); // remove
+                        Ok(())
+                    } else {
+                        Err(ErrorC::DeviceInRoomNotExists(a_device.to_owned()))
+                    }
+                }
+                None => Err(ErrorC::ErrorOther),
+            }
+        } else {
+            Err(ErrorC::RoomNotExists(a_room.to_string()))
+        }
+    }
+    fn delete_room(&mut self, a_room: &str) -> Result<(usize, usize), ErrorC> {
+        if let Some(n_room) = self.test_whether_room_exists(a_room) {
+            let prev_size = self.rooms.len();
+            self.rooms.swap_remove(n_room);
+            let cur_size = self.rooms.len();
+            Ok((prev_size, cur_size))
+        } else {
+            Err(ErrorC::RoomNotExists(a_room.to_string()))
+        }
+    }
+    fn get_all_rooms(&self) -> Option<String> {
+        let mut out_string = String::new();
+        for (i, room) in self.rooms.iter().enumerate() {
+            fmt::write(
+                &mut out_string,
+                format_args!("room {}: {}, ", i, room.get_room_name()),
+            )
+            .expect("error while writing to_string"); // how return None?
+        }
+        if out_string.is_empty() {
+            None
+        } else {
+            Some(out_string)
+        }
+    }
     fn test_whether_room_exists(&self, a_room: &str) -> Option<usize> {
         if self.rooms.iter().any(|x| x.get_room_name() == a_room) {
             self.rooms.iter().position(|x| x.get_room_name() == a_room)
@@ -65,18 +148,36 @@ impl SmartHouse {
         }
     }
 
-    fn append_dev_to_a_room(
+    pub fn append_dev_to_a_room(
         &mut self,
         a_room: &str,
         a_device: &Rc<RefCell<dyn Device>>,
     ) -> Result<Device_Handler, ErrorC> {
         if let Some(room_pos) = self.test_whether_room_exists(a_room) {
-            let x = self.rooms.get(room_pos).unwrap().add_device(a_device);
-            Ok(Device_Handler::new(a_device))
+            if let Ok(_) = self.rooms.get(room_pos).unwrap().add_device(a_device) {
+                Ok(Device_Handler::new(a_device))
+            } else {
+                Err(ErrorC::DeviceInRoomExists(
+                    a_device.borrow().get_name().clone(),
+                ))
+            }
         } else {
             Err(ErrorC::RoomNotExists(a_room.to_string()))
         }
     }
+
+    fn get_devices_in_room(&self, a_room: &str) -> Result<String, ErrorC> {
+        if let Some(room_pos) = self.test_whether_room_exists(a_room) {
+            if let Some(dev_list) = self.rooms.get(room_pos).unwrap().get_all_devices() {
+                Ok(dev_list)
+            } else {
+                Err(ErrorC::ErrorOther)
+            }
+        } else {
+            Err(ErrorC::RoomNotExists(a_room.to_string()))
+        }
+    }
+
     fn change_dev_state_in_room(
         &mut self,
         a_room: &str,
@@ -102,11 +203,12 @@ impl SmartHouse {
 
 trait RoomObj {
     fn get_room_name(&self) -> &str;
-    fn add_device(&self, some_dev: &Rc<RefCell<dyn Device>>);
+    fn add_device(&self, some_dev: &Rc<RefCell<dyn Device>>) -> Result<(), ErrorC>;
     fn find_dev_name(&self, name: &str) -> Option<usize>;
     fn change_dev_state(&self, state: bool, name: &str);
+    fn get_all_devices(&self) -> Option<String>;
+    fn as_any(&self) -> &dyn Any;
 }
-
 struct Room_Generic {
     name: String,
     devices: Box<RefCell<Vec<Rc<RefCell<dyn Device>>>>>,
@@ -116,8 +218,18 @@ impl RoomObj for Room_Generic {
     fn get_room_name(&self) -> &str {
         self.name.as_str()
     }
-    fn add_device(&self, some_dev: &Rc<RefCell<dyn Device>>) {
-        self.devices.as_ref().borrow_mut().push(Rc::clone(some_dev));
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn add_device(&self, some_dev: &Rc<RefCell<dyn Device>>) -> Result<(), ErrorC> {
+        if let Some(_) = self.find_dev_name(some_dev.borrow().get_name().as_ref()) {
+            Err(ErrorC::DeviceInRoomExists(
+                some_dev.borrow().get_name().clone(),
+            ))
+        } else {
+            self.devices.as_ref().borrow_mut().push(Rc::clone(some_dev));
+            Ok(())
+        }
     }
     fn find_dev_name(&self, name: &str) -> Option<usize> {
         if self
@@ -137,6 +249,22 @@ impl RoomObj for Room_Generic {
             )
         } else {
             None
+        }
+    }
+
+    fn get_all_devices(&self) -> Option<String> {
+        let mut out_string = String::new();
+        for (j, dev) in self.devices.as_ref().borrow().iter().enumerate() {
+            fmt::write(
+                &mut out_string,
+                format_args!("dev №{},name: {} ", j, dev.borrow().get_name()),
+            )
+            .expect("error while writing");
+        }
+        if out_string.is_empty() {
+            None
+        } else {
+            Some(out_string)
         }
     }
     fn change_dev_state(&self, state: bool, name: &str) {
@@ -181,6 +309,13 @@ impl Device_Handler {
             Ok(rez.borrow().get_state())
         } else {
             Err(ErrorC::ErrorGettingState)
+        }
+    }
+    fn delete_device(&self) -> Result<(), ErrorC> {
+        if let Some(handle) = self.dev.upgrade() {
+            unimplemented!()
+        } else {
+            Err(ErrorC::ErrorHandleOperation)
         }
     }
 }
@@ -234,6 +369,16 @@ mod tests {
         assert!(sh.append_dev_to_a_room("room1", &dev).is_ok());
     }
     #[test]
+    fn exists_dev_append() {
+        let mut sh = SmartHouse::new();
+        assert!(sh.append_room("room1").is_ok());
+        let dev: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev0".to_string())));
+        assert!(sh.append_dev_to_a_room("room1", &dev).is_ok());
+        assert!(sh.append_dev_to_a_room("room1", &dev).is_err());
+    }
+
+    #[test]
     fn add_to_n_exists_room() {
         let mut sh = SmartHouse::new();
         assert!(sh.append_room("room2").is_ok());
@@ -255,5 +400,104 @@ mod tests {
         assert!(handler1.get_state().is_ok_and(|rez| rez == true));
         assert!(sh.change_dev_state_in_room("room1", "dev0", false).is_ok());
         assert!(handler1.get_state().is_ok_and(|rez| rez == false));
+    }
+    #[test]
+    fn test_get_devices_in_room() {
+        let mut sh = SmartHouse::new();
+        assert!(sh.append_room("room1").is_ok());
+        let dev: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev0".to_string())));
+
+        let dev1: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev1".to_string())));
+        let rez_handler = sh.append_dev_to_a_room("room1", &dev);
+        let rez_handler1 = sh.append_dev_to_a_room("room1", &dev1);
+        let dev_list = sh.get_devices_in_room("room1");
+        assert!(dev_list.is_ok());
+        println!("------------------->devices: {}", dev_list.unwrap());
+    }
+    #[test]
+    fn test_get_all_rooms() {
+        let mut sh = SmartHouse::new();
+        let room1 = "room1".to_string();
+        let room2 = "room2".to_string();
+        assert!(sh.append_room(&room1).is_ok());
+        assert!(sh.append_room(&room2).is_ok());
+        assert!(sh.get_all_rooms().is_some());
+        println!("rooms--------> {}", sh.get_all_rooms().unwrap())
+    }
+    #[test]
+    fn test_delete_room_ne() {
+        let mut sh = SmartHouse::new();
+        let room1 = "room1".to_string();
+        let room2 = "room2".to_string();
+        let room3 = "room3".to_string();
+        assert!(sh.append_room(&room1).is_ok());
+        assert!(sh.append_room(&room2).is_ok());
+        assert!(sh.append_room(&room3).is_ok());
+        assert!(sh.delete_room("room4").is_err())
+    }
+    #[test]
+    fn test_delete_room() {
+        let mut sh = SmartHouse::new();
+        let room1 = "room1".to_string();
+        let room2 = "room2".to_string();
+        let room3 = "room3".to_string();
+        assert!(sh.append_room(&room1).is_ok());
+        assert!(sh.append_room(&room2).is_ok());
+        assert!(sh.append_room(&room3).is_ok());
+        assert!(sh.delete_room("room3").is_ok_and(|x| x == (3, 2)));
+    }
+    #[test]
+    fn test_delete_dev() {
+        let mut sh = SmartHouse::new();
+        let room1 = "room1".to_string();
+        let room2 = "room2".to_string();
+        assert!(sh.append_room(&room1).is_ok());
+        assert!(sh.append_room(&room2).is_ok());
+
+        let dev0: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev0".to_string())));
+        let dev1: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev1".to_string())));
+
+        let _dev0_handler = sh.append_dev_to_a_room("room1", &dev0);
+        let _dev1_handler = sh.append_dev_to_a_room("room1", &dev1);
+        assert!(sh
+            .get_device_number_in_room("room1")
+            .is_some_and(|n| n == 2));
+        assert!(sh.delete_device(&room1, "dev1").is_ok());
+        assert!(sh
+            .get_device_number_in_room("room1")
+            .is_some_and(|n| n == 1));
+        println!("{}", sh.get_dev_report(&dev0).unwrap());
+    }
+    #[test]
+    fn test_report_dev() {
+        let mut sh = SmartHouse::new();
+        let room1 = "room1".to_string();
+        let room2 = "room2".to_string();
+        assert!(sh.append_room(&room1).is_ok());
+        assert!(sh.append_room(&room2).is_ok());
+
+        let dev0: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev0".to_string())));
+        let dev1: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev1".to_string())));
+
+        let _dev0_handler = sh.append_dev_to_a_room("room1", &dev0);
+        let _dev1_handler = sh.append_dev_to_a_room("room1", &dev1);
+
+        let dev3: Rc<RefCell<dyn Device>> =
+            Rc::new(RefCell::new(Example_Device::new("dev1".to_string())));
+
+        let _dev3_handler = sh.append_dev_to_a_room("room2", &dev3);
+        assert!(sh
+            .get_device_number_in_room("room1")
+            .is_some_and(|n| n == 2));
+        assert!(sh
+            .get_device_number_in_room("room2")
+            .is_some_and(|n| n == 1));
+        println!("REPORT IS {}", sh.get_dev_report(&dev1).unwrap());
     }
 }

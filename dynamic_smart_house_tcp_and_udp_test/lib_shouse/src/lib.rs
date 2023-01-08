edf23c5ffe34c2181b633291a475b26d1b584575
Kerosin3 <keromvp@gmail.com>
2023-01-08 18:08:32 +0300

    added test

---
+++ dynamic_smart_house_tcp_and_udp_test/lib_shouse/src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(associated_type_defaults)]
+#![feature(mutex_unlock)]
+#![feature(is_some_and)]
+#[allow(unused_imports)]
+#[allow(unused_variables)]
+#[allow(dead_code)]
+#[allow(clippy::manual_map)]
+pub mod home;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
---
+++ dynamic_smart_house_tcp_and_udp_test/server_shouse/src/server/mod.rs
@@ -0,0 +1,3 @@
+pub mod server_socket_struct;
+pub mod server_tcp_loop;
+pub mod server_termometer_struct;
